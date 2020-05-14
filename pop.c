4f955ff7ab9bd79b57f3233094e46f7aea0195e4
Juan Jose <1561@holbertonschool.com>
2020-05-14 17:53:05 +0000

    [add] first version

---
+++ pop.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * pop - Will delete in the value on top of the stack
+ * @stack: pointer to top of the stack
+ * @line_number: the line that will run
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = NULL;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILIURE);
+	}
+	tmp = *stack;
+	*stack = tmp->next;
+	if (tmp->next)
+	{
+		tmp->next->prev = NULL;
+	}
+	free(tmp);
+}
+
+	
